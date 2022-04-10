b72ef49f6934734b68a1073e46ab7053f99d9273
Pushkar Sane <78395956+SurturFTW@users.noreply.github.com>
2022-04-10 19:56:48 +0530

    Update SIMPLEHU.C

--- SIMPLEHU.C
+++ SIMPLEHU.C
@@ -1,30 +1,22 @@
 #include<graphics.h>
 #include<conio.h>
-int main()
-{
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-setbkcolor(2);
-setcolor(4);
-circle(498,134,10);
-setfillstyle(1,BLUE);
-floodfill(489,134,4);
-line(500,81,463,164);
-line(463,164,528,165);
-line(528,165,500,81);
-line(466,165,468,245);
-line(468,245,532,244);
-line(532,244,525,165);
-line(525,165,527,164);
+ 
+void main(){
+    int gd = DETECT,gm;
+    initgraph(&gd, &gm, "C:\\TC\\BGI");
+    setcolor(WHITE);
+    rectangle(150,180,250,300);
+    rectangle(250,180,420,300);
+    rectangle(180,250,220,300);
 
-line(503,85,614,80);
-line(614,80,646,159);
-line(646,159,528,165);
-line(528,165,500,81);
-
-line(642,243,532,249);
-line(532,249,525,166);
-getch();
-closegraph();
-return 0;
-}
+    setfillstyle(SOLID_FILL, BROWN);
+    floodfill(152, 182, WHITE);
+    floodfill(252, 182, WHITE);
+    
+    line(200,100,150,180);
+    line(200,100,250,180);
+    line(200,100,370,100);
+    line(370,100,420,180);
+    getch();
+    closegraph();
+} 
