a259b6f0fbbdd0b05c5b82b2b40f222642ffad9b
Pushkar Sane <78395956+SurturFTW@users.noreply.github.com>
2022-04-10 20:31:54 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,33 +1,38 @@
 #include <graphics.h>
 
 void main(){
-    int gm = DETECT,gd;
-    float a = 200,b= 200,x,y,r =40;
-    float p1;
-    initgraph(&gm,&gd,"C://TC/BGI");
-    x = 0; y = r;
+    int gd = DETECT,gm;
+    float a,b,x,y,r,p1;
+    initgraph(&gd,&gm,"C://TURBOC3//BGI");
+    printf("enter a and b :");
+    scanf("%f %f",&a,&b);
+    printf("enter radius :");
+    scanf("%f",&r);
+
+    x = 0;
+    y = r;
     p1 = 5/4 - r;
     while(x<=y){
-        delay(1000);
+        delay(100);
         putpixel(a+x,b+y,3);
         putpixel(a+x,b-y,3);
         putpixel(a-x,b+y,3);
         putpixel(a-x,b-y,3);
         //symmetry
-        delay(1000);
+        delay(100);
         putpixel(a+y,b+x,3);
         putpixel(a+y,b-x,3);
         putpixel(a-y,b+x,3);
         putpixel(a-y,b-x,3);
 
     if(p1 <= 0){
-        p1 += 2 * x + 1;
-    }
+        p1 =p1+ 2 * x + 1;
+        }
     else{
-        p1 += 2x - 2 y + 1;
+        p1 =p1+ 2*x - 2*y + 1;
         y--;
         }
     x++;
     }
-  return 0;
+    getch();
 }
