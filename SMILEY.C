b99d8a58cf5e7ccc619e7e16e41b470bc0703d68
Pushkar Sane <78395956+SurturFTW@users.noreply.github.com>
2022-04-10 19:45:52 +0530

    Update SMILEY.C

--- SMILEY.C
+++ SMILEY.C
@@ -1,24 +1,19 @@
-#include<graphics.h>
-#include<conio.h>
-int main()
-{
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-setcolor(YELLOW);
-circle(210,176,70);
-circle(185,152,10);
-circle(240,150,10);
-arc(210,176,180,350,50);
-setfillstyle(1,YELLOW);
-floodfill(210,176,YELLOW);
-setcolor(BLACK);
-arc(210,176,180,360,49);
-arc(210,176,180,360,48);
-arc(210,176,180,360,47);
-arc(210,176,180,360,46);
+#include <graphics.h>
 
-
-getch();
-closegraph();
-return 0;
+void main(){
+	int gr = DETECT, gm;
+	initgraph(&gr, &gm, "C:\\Turboc3\\BGI");
+	setcolor(YELLOW);
+	circle(300, 100, 40);
+	setfillstyle(SOLID_FILL, YELLOW);
+	floodfill(300, 100, YELLOW);
+	setcolor(BLACK);
+	setfillstyle(SOLID_FILL, BLACK);
+	fillellipse(310, 85, 2, 6);
+	fillellipse(290, 85, 2, 6);
+	ellipse(300, 100, 205, 335, 20, 9);
+	ellipse(300, 100, 205, 335, 20, 10);
+	ellipse(300, 100, 205, 335, 20, 11);
+	getch();
+	closegraph();
 }
